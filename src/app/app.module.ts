d9ed872009c00fb9dfc3961f44c09b46a22034e5
JoTa <juan.caro.1987@gmail.com>
2022-07-08 02:14:09 -0500

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { TransportistaComponent } from './components/transportista/transportista.component';
+import { EmpresaComponent } from './components/empresa/empresa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TransportistaComponent,
+    EmpresaComponent
   ],
   imports: [
     BrowserModule,
