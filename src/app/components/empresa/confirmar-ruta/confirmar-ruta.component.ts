5f4023b05edb0f56b37bf4608ba2ec43c9a7e5ec
JoTa <juan.caro.1987@gmail.com>
2022-07-18 01:46:56 -0500

    F

--- src/app/components/empresa/confirmar-ruta/confirmar-ruta.component.ts
+++ src/app/components/empresa/confirmar-ruta/confirmar-ruta.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+import mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-confirmar-ruta',
   templateUrl: './confirmar-ruta.component.html',
-  styleUrls: ['./confirmar-ruta.component.sass']
+  styleUrls: ['./confirmar-ruta.component.sass'],
 })
-export class ConfirmarRutaComponent implements OnInit {
+export class ConfirmarRutaComponent implements OnInit, AfterViewInit {
+  @ViewChild('mapDiv') mapDivElement!: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: this.mapDivElement.nativeElement, //'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+      //projection: 'globe', // display the map as a 3D globe
+    });
   }
-
 }
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibGwtam90YS1sbCIsImEiOiJjbDVxY2JpMGEwN2NrM2twZWEzYmx4YWowIn0.-crxzsUzYyAI94jawksecQ';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
